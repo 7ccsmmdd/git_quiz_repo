659a602dc8d9d525167f000c1cbb3984ec4feb80
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2020-11-17 10:21:10 +0000

    Code updated

--- git_quiz_project_1/src/git_quiz_project_1/MainClass.java
+++ git_quiz_project_1/src/git_quiz_project_1/MainClass.java
@@ -5,7 +5,7 @@ import git_quiz_project_2.Calculator;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Result is " + Calculator.calculate(7, 3));
+		System.out.println("Result is " + Calculator.calculate(10, 3));
 	}
 
 }
--- git_quiz_project_2/src/git_quiz_project_2/Calculator.java
+++ git_quiz_project_2/src/git_quiz_project_2/Calculator.java
@@ -2,6 +2,6 @@ package git_quiz_project_2;
 
 public class Calculator {
 	public static int calculate(int param1, int param2) {
-		return param1 * param2 + 77;
+		return param1 * param2 + 20;
 	}
 }
