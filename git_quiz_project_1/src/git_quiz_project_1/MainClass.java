4fdf0eb81da83abbea4afc54c64e0f0c70dc06c6
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2020-11-17 10:19:47 +0000

    Initial commit

---
+++ git_quiz_project_1/src/git_quiz_project_1/MainClass.java
@@ -0,0 +1,11 @@
+package git_quiz_project_1;
+
+import git_quiz_project_2.Calculator;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Result is " + Calculator.calculate(7, 3));
+	}
+
+}
---
+++ git_quiz_project_2/src/git_quiz_project_2/Calculator.java
@@ -0,0 +1,7 @@
+package git_quiz_project_2;
+
+public class Calculator {
+	public static int calculate(int param1, int param2) {
+		return param1 * param2 + 77;
+	}
+}
